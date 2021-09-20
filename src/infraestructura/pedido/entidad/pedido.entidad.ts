ecfd70fbcfb1bfc56dc62ac9b01704f17add1880
Maria Alejandra Bohorquez Martheyn - Ceiba Software <maria.bohorquez@ceiba.com.co>
2021-09-20 09:44:01 -0500

    se borra variables sin utilizar.

--- src/infraestructura/pedido/entidad/pedido.entidad.ts
+++ src/infraestructura/pedido/entidad/pedido.entidad.ts
@@ -1,5 +1,4 @@
-import { PedidoProductoEntidad } from 'src/infraestructura/pedido-producto/entidad/pedido-producto.entidad';
-import { Column, Entity, OneToMany, PrimaryGeneratedColumn } from 'typeorm';
+import { Column, Entity,  PrimaryGeneratedColumn } from 'typeorm';
 
 @Entity({ name: 'pedido' })
 export class PedidoEntidad {
